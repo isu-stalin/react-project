f77d7fb1e4295473057213f6b582185cdef7631e
Xamrayev Xasan <xamrayevxasan11@gmail.com>
2024-10-26 13:31:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <header>
+          <div className="oval">
+            <img
+              src="https://luckyszakul0.github.io/FM-Social-links-profile/assets/images/avatar-jessica.jpeg"
+              alt="avatar"
+            />
+          </div>
+          <div className="underHeader">
+            <h2>Jessica Randall</h2>
+            <h3 className="location">London, United Kingdom</h3>
+          </div>
+          <div className="about">
+            <p>"Front-end developer and avid reader."</p>
+          </div>
+        </header>
+        <main>
+          <button>GitHub</button>
+
+          <button>Frontend Mentor</button>
+
+          <button>LinkedIn</button>
+
+          <button>Twitter</button>
+
+          <button>Instagram</button>
+        </main>
+      </div>
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Challenge by{" "}
+          <a href="#" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="#" target="_blank">
+            luckyszakul0
+          </a>
+          .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
